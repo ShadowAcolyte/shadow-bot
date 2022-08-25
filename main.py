81095d6018d7d392431ba5359dbce7a5bd3fd376
Samvrit <samvrit8703@gmail.com>
2022-08-25 23:02:55 +0530

    updates

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from dis import disco
+from email import message
 import discord
 import os
 from macro import macro_list
@@ -8,6 +10,12 @@ intents.message_content = True
 
 bot = commands.Bot("$", intents = intents)
 
+async def get_message_reference(message: discord.Message) -> discord.Message:
+    if message.reference.cached_message is not None:
+        return message.reference.cached_message
+    else:
+        return await message.channel.fetch_message(message.reference.message_id)
+
 @bot.listen('on_message')
 async def on_message(message: discord.Message):
     # macros
@@ -15,11 +23,9 @@ async def on_message(message: discord.Message):
         if macro_content := macro_list.get(message.content[2:]):
             if message.reference is None:
                 await message.reply(macro_content)
-            elif message.reference.cached_message is None:
-                ref_message: discord.Message = await message.channel.fetch_message(message.reference.message_id)
-                await ref_message.reply(macro_content)
             else:
-                await message.reference.cached_message.reply(macro_content)
+                ref_message: discord.Message = await get_message_reference(message)
+                await ref_message.reply(macro_content)
 
     # good bot messages
     if message.content.lower().startswith("good bot") and message.reference.cached_message.author.id == bot.user.id:
@@ -30,6 +36,11 @@ async def on_message(message: discord.Message):
 
 @bot.command()
 async def say(ctx: commands.Context, *, arg: str):
-    await ctx.send(arg)
+    await ctx.message.delete();
+    if ctx.message.reference is None:
+        await ctx.send(arg)
+    else:
+        ref_message: discord.Message = await get_message_reference(ctx.message)
+        await ref_message.reply(arg)
 
 bot.run(os.environ['DISCORD_TOKEN'])

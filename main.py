4d8be318076f2c12d7eaa497400b414062f090bc
Samvrit <samvrit8703@gmail.com>
2022-08-25 22:34:37 +0530

    change bot prefix to '$'

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot("sh ", intents = intents)
+bot = commands.Bot("$", intents = intents)
 
 @bot.listen('on_message')
 async def on_message(message: discord.Message):

558e18541711396de87d75266b790cb6e9945e83
Samvrit <samvrit8703@gmail.com>
2022-08-26 17:42:48 +0530

    added error logging to channel

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 import discord
 import os
+import datetime
+import traceback
 from macro import macro_list
 from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
+bot_prefix = '$$'
 
-bot = commands.Bot("$", intents = intents)
+bot = commands.Bot(bot_prefix, intents = intents)
 
 async def get_message_reference(message: discord.Message) -> discord.Message:
     if message.reference.cached_message is not None:
         return message.reference.cached_message
     else:
         return await message.channel.fetch_message(message.reference.message_id)
+
+async def bot_log_info(msg: str):
+    await bot.get_channel(1012684225553637476).send(f'`{datetime.datetime.now().strftime("[%Y/%m/%d, %H:%M:%S]")} [info] {msg}`')
+
+async def bot_log_err(msg: str):
+    await bot.get_channel(1012684225553637476).send(f'`{datetime.datetime.now().strftime("[%Y/%m/%d, %H:%M:%S]")} [error] {msg}`')
+
+@bot.listen('on_ready')
+async def on_ready():
+    await bot_log_info('Shadow bot v0.3 is now online!')
+    await bot_log_info(f'Using prefix {bot_prefix}')
 
 @bot.listen('on_message')
 async def on_message(message: discord.Message):
@@ -33,8 +47,16 @@ async def on_message(message: discord.Message):
     if message.content.lower().startswith("bad bot") and message.reference is not None and ref_message.author.id == bot.user.id:
         await message.reply("Sowwy! o(╥﹏╥)~")
 
+@bot.event
+async def on_command_error(ctx, error):
+    error = getattr(error, 'original', error)
+    output = ''.join(traceback.format_exception(error))
+    await bot_log_err(output)
+    print(output)
+
 @bot.command()
 async def say(ctx: commands.Context, *, arg: str):
+    print(1/0)
     await ctx.message.delete()
     everyone = ctx.channel.permissions_for(ctx.author).mention_everyone
     if ctx.message.reference is None:

